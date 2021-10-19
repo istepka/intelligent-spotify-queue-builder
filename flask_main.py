f73768d96b55c6997a6ecdd8847819980c6586c2
Iggyyy <ignacy.stepka@gmail.com>
2021-10-19 15:56:57 +0200

    basic site functionalities

--- flask_main.py
+++ flask_main.py
@@ -1,70 +1,65 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
 from werkzeug.utils import redirect
+from scripts import setup
+from classes.downloader import Downloader
+from classes.tracks import Track
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+
+
 db = SQLAlchemy(app)
+class TrackData(db.Model):
+    id = db.Column(db.String(200), primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    artist = db.Column(db.String(200), default='no data')
+    album = db.Column(db.String(200), default='no data')
 
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
         return '<Task %r>' % self.id
 
     #type db.create_all() to create database
 
+def search_for_track(name):
+
+    #setup which requires properly created 'credentials.txt' file
+    setup.prep_env_from_file()
+    username  = setup.get_spotify_username()
+
+    #create downloader instance for user
+    downloader = Downloader(username)
+
+    #download desired track data by name
+   
+    track = Track(downloader.fetch_single_track_by_name(name))
+    td = TrackData(id=track.id, name=track.name, artist=track.artist_name, album=track.album_name)
+    db.session.add(td)
+    db.session.commit()
+   
+
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_todo = Todo(content=task_content)
+        task_content = request.form['search_track']
 
-        try:
-            db.session.add(new_todo)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+        if len(task_content) > 0:
+            try: 
+                search_for_track(task_content)
+            except:
+                return 'There was an issue searching for your track'
 
+        tracks = TrackData.query.order_by(TrackData.name).all()
+        return render_template('index.html', tracks=tracks)
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+        return render_template('index.html')
 
-
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
-
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+   
---
+++ test.py
@@ -0,0 +1,5 @@
+from classes.tracks import Track
+from classes.downloader import Downloader
+
+
+d = Downloader()
